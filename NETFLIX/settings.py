d8b67fcdcbb8f0139d3032838329fa86056c7dd6
Josue Baldeon Araujo <136621348+Klell11@users.noreply.github.com>
2024-12-16 20:51:29 -0500

    conf static

--- NETFLIX/settings.py
+++ NETFLIX/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'whitenoise.runserver_nostatic',
     'aplicaciones.Login'
 ]
 
@@ -127,6 +128,8 @@ STATIC_URL = 'static/'
 if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
